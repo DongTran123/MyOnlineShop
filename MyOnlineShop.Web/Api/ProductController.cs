cd405aa79e5a75e33830fb334dbaff2180bda459
TEO\bmn <tranhadong1991@gmail.com>
2017-06-05 08:40:47 +0700

    Product WebApi

---
+++ MyOnlineShop.Web/Api/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using MyOnlineShop.Service;
+using MyOnlineShop.Web.Infrastructure.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyOnlineShop.Web.Api
+{
+    [RoutePrefix("api/product")]
+    public class ProductController : ApiControllerBase
+    {
+        IProductService _IProductService;
+
+        public ProductController(IErrorService IErrorService, IProductService IProductService) : base(IErrorService)
+        {
+            this._IProductService = IProductService;
+        }
+        [Route("getall")]
+        public HttpResponseMessage Get(HttpRequestMessage request)
+        {
+            return CreateHttpResponse(request, () => 
+            {
+                HttpResponseMessage response = null;
+                if (ModelState.IsValid)
+                {
+                    request.CreateErrorResponse(HttpStatusCode.BadRequest, ModelState);
+
+                }
+                else
+                {
+                    var ListProduct = _IProductService.GetAll();
+                    response = request.CreateResponse(HttpStatusCode.OK, ListProduct);
+                }
+                return response;
+            });
+        }
+    }
+}
--- MyOnlineShop.Web/Infrastructure/Core/ApiControllerBase.cs
+++ MyOnlineShop.Web/Infrastructure/Core/ApiControllerBase.cs
@@ -2,6 +2,8 @@
 using MyOnlineShop.Service;
 using System;
 using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
@@ -22,6 +24,19 @@ namespace MyOnlineShop.Web.Infrastructure.Core
             {
                 response = function.Invoke();
             }
+            catch (DbEntityValidationException ex)
+            {
+                foreach (var eve in ex.EntityValidationErrors)
+                {
+                    Trace.WriteLine($"Entity of type \"{eve.Entry.Entity.GetType().Name}\" in state \"{eve.Entry.State}\" has the following validation error.");
+                    foreach (var ve in eve.ValidationErrors)
+                    {
+                        Trace.WriteLine($"- Property: \"{ve.PropertyName}\", Error: \"{ve.ErrorMessage}\"");
+                    }
+                }
+                LogError(ex);
+                response = requestMessage.CreateResponse(HttpStatusCode.BadRequest, ex.InnerException.Message);
+            }
             catch (DbUpdateException dbEx)
             {
                 LogError(dbEx);
@@ -32,7 +47,7 @@ namespace MyOnlineShop.Web.Infrastructure.Core
                 LogError(ex);
                 response = requestMessage.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
             }
-            return null;
+            return response;
         }
 
         private void LogError(Exception ex)
